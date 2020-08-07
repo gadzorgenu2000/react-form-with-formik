d08a0fc46c7b3bfd468591351d7b58dd4a796e59
gadzorgenu2000 <42786156+gadzorgenu2000@users.noreply.github.com>
2020-08-07 11:49:30 +0000

    making changes to the form

--- src/Form/form.js
+++ src/Form/form.js
@@ -1,5 +1,5 @@
 import React,{ Fragment} from 'react';
-import { Box,Input,Flex,Radio ,Select,FormControl, FormLabel,Text,Button} from '@chakra-ui/core'
+import { Box,Input,Flex,Radio ,Select,FormControl, FormLabel,Text,Button, Grid} from '@chakra-ui/core'
 // import {ColorModeProvider, theme, CSSReset  } from "@chakra-ui/core";
 function Form() {
     // const {colorMode} =useColorMode();
@@ -8,47 +8,47 @@ function Form() {
     <>
     <Fragment>
       <Flex h="100vh" align="center" justify="center">
-          <Box w={["70%","65%","60%"]} p="20px" bgImage="url('/Image/logo.jpg')" bgRepeat="no-repeat" bgPos="center"
-           rounded="lg" boxShadow="lg" borderWidth={2}>
-            <Flex mt="15px" >
-              <Radio w="42%" value="one-way">One way</Radio>
-              <Radio   px="20" value="round-trip">Round Trip</Radio>
+          <Box w={["100%","60%"]} h={["65%","65%"]}p="15px" bg="#808080" 
+           rounded="lg" boxShadow="lg" borderWidth={2} >
+            <Flex mb="30px" mt="30px" >
+              <Radio color="white" value="one-way">One way</Radio>
+              <Radio color="white"  px="20"  value="round-trip">Round Trip</Radio>
             </Flex>
-            <Flex mt="20px" justifyContent="space-between">
+            <Flex mb="40px"mt="30px" justifyContent="space-between">
               <FormControl w="50%">
-                <FormLabel htmlFor="From">From</FormLabel>
+                <FormLabel color="white"fontWeight="bold" htmlFor="From">From</FormLabel>
                   <Select id="from" placeholder="Select Depature" />
               </FormControl>
               <FormControl w="50%" px="15px">
-                <FormLabel htmlFor="to">To</FormLabel>
+                <FormLabel  color="white" fontWeight="bold" htmlFor="to" >To</FormLabel>
                   <Select id="to" placeholder="Select Depature" />
               </FormControl>
             </Flex>
-            <Flex mt="20px" >
+            <Flex mt="30px" >
               <FormControl w="50%">
-                <FormLabel htmlFor="depature-date">Depature Date</FormLabel>               
+                <FormLabel color="white"  htmlFor="depature-date">Depature Date</FormLabel>               
                  <Input type="date" placeholder="dd/mm/yy"/>
               </FormControl>
               <FormControl w="50%"  px="15px">
-                <FormLabel htmlFor="return-date">Return Date</FormLabel>
+                <FormLabel color="white" htmlFor="return-date">Return Date</FormLabel>
                 <Input type="date" placeholder="dd/mm/yy"/>
               </FormControl>
             </Flex>
-            <Text mt="20px" fontWeight="bold">Passenger Information</Text>
-            <Flex mt="5px" justifyContent="space-around">
-              <FormControl  w="30%">
-                <FormLabel htmlFor="adult">Adult (12+ yrs)</FormLabel>
+            <Text mt="30px"color="white" fontWeight="bold">Passenger Information</Text>
+            <Grid templateColumns={{md: "repeat(3, 1fr)"}} gap={6} mt="5px" >
+              <FormControl  >
+                <FormLabel color="white" fontWeight="bold" htmlFor="adult">Adult (12+ yrs)</FormLabel>
                 <Select id="to" placeholder="12" />
               </FormControl>
-              <FormControl  w="30%">
-                <FormLabel htmlFor="child">Child (2-12yrs)</FormLabel>
+              <FormControl >
+                <FormLabel color="white" fontWeight="bold" htmlFor="child">Child (2-12yrs)</FormLabel>
                 <Select id="to" placeholder="2" />
               </FormControl>
-              <FormControl w="30%">
-                <FormLabel htmlFor="infant">Infant</FormLabel>
+              <FormControl >
+                <FormLabel color="white" fontWeight="bold" htmlFor="infant">Infant</FormLabel>
                 <Select id="to" placeholder="0" />
               </FormControl>
-            </Flex>
+            </Grid>
             <Flex justify="center">
               <Button mt="15px" type="submit" variantColor="orange">Search Flights</Button>
             </Flex>
